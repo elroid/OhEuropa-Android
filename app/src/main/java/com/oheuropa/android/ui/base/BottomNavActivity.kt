ff9e0db89723dd5f0e7f972b5d22a77348b43626
Elliot Long <e@elroid.com>
2018-09-21 09:16:58 +0100

    Moved to support-lib bottom navigation
    Ran into build issues with existing 3rd party lib
    Also removed background for better support across multiple screen sizes

--- app/src/main/java/com/oheuropa/android/ui/base/BottomNavActivity.kt
+++ app/src/main/java/com/oheuropa/android/ui/base/BottomNavActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.design.widget.BottomNavigationView
 import android.view.MenuItem
-import androidx.core.view.doOnPreDraw
 import com.github.ajalt.timberkt.i
 import com.github.ajalt.timberkt.v
 import com.oheuropa.android.R
 import com.oheuropa.android.ui.compass.CompassActivity
 import com.oheuropa.android.ui.info.InfoActivity
 import com.oheuropa.android.ui.map.MapActivity
-import com.oheuropa.android.util.ViewUtils
 import kotlinx.android.synthetic.main.bottom_navigation.*
 
 /**
@@ -23,7 +21,9 @@ import kotlinx.android.synthetic.main.bottom_navigation.*
  * @author <a href="mailto:e@elroid.com">Elliot Long</a>
  *         Copyright (c) 2018 Elroid Ltd. All rights reserved.
  */
-abstract class BottomNavActivity : BaseActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+abstract class BottomNavActivity : BaseActivity(),
+		BottomNavigationView.OnNavigationItemSelectedListener,
+		BottomNavigationView.OnNavigationItemReselectedListener {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		i { "onCreate $this" }
@@ -31,17 +31,8 @@ abstract class BottomNavActivity : BaseActivity(), BottomNavigationView.OnNaviga
 		setContentView(getLayoutId())
 
 		//set up bottom nav
-		bottomNavigationView.onNavigationItemSelectedListener = this
-		bottomNavigationView.setTextVisibility(false)
-		val tabBarBgDrawable = bottomNavigationView.background
-		val iw = tabBarBgDrawable.intrinsicWidth
-		val ih = tabBarBgDrawable.intrinsicHeight
-		val sw = ViewUtils.getScreenWidth()
-		val th = ViewUtils.getHeightAtWidth(iw, ih, sw)
-		bottomNavigationView.doOnPreDraw {
-			bottomNavigationView.itemHeight = th
-			bottomNavigationView.setIconsMarginTop(ViewUtils.dpToPx(12))
-		}
+		bottomNavigationView.setOnNavigationItemSelectedListener(this)
+		bottomNavigationView.setOnNavigationItemReselectedListener(this)
 	}
 
 	@LayoutRes
@@ -53,20 +44,24 @@ abstract class BottomNavActivity : BaseActivity(), BottomNavigationView.OnNaviga
 		v { "this tab pressed(%s) - doing nothing by default" }
 	}
 
+	override fun onNavigationItemReselected(item: MenuItem) {
+		onThisTabPressed()
+	}
+
 	override fun onNavigationItemSelected(item: MenuItem): Boolean {
 		i { "onNavigationItemSelected(${item.title})" }
 		if (item.itemId == getNavigationMenuItemId())
 			onThisTabPressed()
 		else
 			bottomNavigationView.postDelayed({
-				i { "creating intent..." }
+				v { "creating intent..." }
 				when (item.itemId) {
 					R.id.navigation_compass -> startActivity(CompassActivity.createIntent(getCtx()))
 					R.id.navigation_map -> startActivity(MapActivity.createIntent(getCtx()))
 					R.id.navigation_info -> startActivity(InfoActivity.createIntent(getCtx()))
 				}
 				finish()
-			}, 300)
+			}, 100)
 		return true
 	}
 
